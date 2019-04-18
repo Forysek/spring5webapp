b06a9ac2054b2d02f9fa1e360e0867c6dd44ecce
Foryssek <foryssek@gmail.com>
2019-04-18 22:54:53 +0200

    First commit

---
+++ src/test/java/com/forysek/spring5webapp/Spring5webappTests.java
@@ -0,0 +1,16 @@
+package com.forysek.spring5webapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5webappTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
